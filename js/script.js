54f218583191d1cbb1641805210e547703234ae4
EvaPerezMartinez <epm0027@alu.medac.es>
2025-08-23 21:17:11 +0200

    estilos

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Botón volver arriba
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
